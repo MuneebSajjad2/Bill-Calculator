cde13ad301714e17b70d0f38b26a2997a883e0b9
Muneeb <muneebsajjad2@gmail.com>
2021-09-14 16:38:30 +0500

    Change the layout and worked on multiple checkbox selection

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React, { useEffect, useState, useRef } from 'react';
 // import Practice1 from "./Practice/Practice2"
 function App() {
   let dishes = [
-    {name:"Biryani", price:150},
+    {name:"Biryani", price:150,ordered:false},
     {name:"Qorma", price:250},
     {name:"Shawarma", price:120},
     {name:"Mutton White Karahi", price:1450},
@@ -14,14 +14,14 @@ function App() {
     {name:"Mutton Polao", price:570},
     {name:"Beef Polao", price:110}
   ]
+   let filteredDishes; 
+  console.log(filteredDishes);
   let showName = useRef();
   let showPrice = useRef();
   let showQty = useRef();
   let showTotal = useRef();
   let discount = useRef();
-  let grandTotal = useRef();
-  
-  
+  let grandTotal = useRef();  
   return (
     <div className="container">
       <h1>Calculate Your Bill</h1>
@@ -35,62 +35,49 @@ function App() {
           }}
           type="number" className="qty"/></td>
           <td><input onChange={(evt)=>{
-            dish.ordered = evt.target.checked
+            if (dish.ordered === false){
+              dish.ordered = evt.target.checked
+            }else if(dish.ordered === true){
+              dish.ordered = false
+            }
+            dish.ordered= evt.target.checked
+            filteredDishes = dishes.filter((dishf)=>{
+              return dishf.ordered
+            })
+            console.log(filteredDishes);
+
+
           }}
           type="checkbox" className="checkbox"></input></td>
         </tr>
-
     })
   }    
   <div className="tableSecondPortion">
   <tr>
     <td >Item</td>
-    <td ref={showName} className="itemName, " colSpan="2" > </td>
-  </tr>
-  <tr>
     <td>Price</td>
-    <td ref ={showPrice} className="itemPrice" colSpan="2"></td>
-    </tr>
-  <tr>
     <td>Qty</td>
-    <td className="itemQTY " ref={showQty} colSpan="2"></td>
-    </tr>
-  <tr>
     <td>Total =</td>
-    <td className="itemTotal" ref={showTotal} colSpan="2"></td>
-    </tr>
-  <tr>
     <td>Disc (10%) =</td>
-    <td className="itemDisc" ref={discount} colSpan="2"></td>
-    </tr>
-  <tr>
     <td>Grand Total =</td>
-    <td className="itemGrandTotal" ref={grandTotal} colSpan="2"></td>
-    </tr>
-  <tr><button onClick={()=>{
-      dishes.forEach(dishItem => {
-        if(dishItem.ordered){
-          showName.current.innerText += dishItem.name;
-          showPrice.current.innerText += parseInt(dishItem.price);
-        }
-        if(dishItem.quantity){
-          showQty.current.innerText = dishItem.quantity;
-        }
-        if(dishItem.quantity && dishItem.ordered){
+  </tr>
+  <tr ref={showName}>
+   
+    <td><ul><li>150</li></ul></td>
+    <td><ul><li>3</li></ul></td>
+    <td><ul><li>450</li></ul></td>
+    <td><ul><li>45</li></ul></td>
+    <td><ul><li>415</li></ul></td>
+  </tr>
   
-          showTotal.current.innerText =  showPrice.current.innerText * showQty.current.innerText;
-        }
-        else if (dishItem.ordered){
-          showTotal.current.innerText =  showPrice.current.innerText
-        }
-        if(dishItem.ordered &&  showTotal.current.innerText > 800){
-         
-          discount.current.innerText = Math.round(showTotal.current.innerText * 0.010);
-        }
-        if(dishItem.ordered && showTotal.current.innerText > 800){
-          grandTotal.current.innerText = (showPrice.current.innerText * showQty.current.innerText) - discount.current.innerText;
-        }
-      });
+  <tr><button onClick={()=>{
+    let mapedDishes = filteredDishes.map((dishM)=>{
+      console.log(dishM.name);
+      return dishM.name
+    })
+    showName.current.innerHTML = mapedDishes.map((sdish)=>{return {sdish}.toString()})
+    
+    console.log(mapedDishes);
   }}>Calculate</button></tr>
   </div>
   </table>
